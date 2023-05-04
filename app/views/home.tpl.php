01bb2ec0c00e1318b591fdd40c8a3331836500ef
Antoine <a-guillot@laposte.net>
2023-05-04 14:43:20 +0200

    remplacement des lorem

--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -5,8 +5,10 @@
     </div>
     <div class="col-md-6">
       <h2>Nettoyage & repassage</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi commodo tellus a augue tempor, quis sagittis eros eleifend. Fusce tristique bibendum turpis in lacinia. Nulla sed purus vel augue sagittis maximus. Sed posuere ultricies purus, ac eleifend ex gravida in. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque euismod enim non risus volutpat vehicula. In hac habitasse platea dictumst.</p>
-    </div>
+      <p>
+      Lorsque vous confiez vos vêtements à Pressim, notre équipe de professionnels expérimentés s'assure que chaque article est traité avec le plus grand soin et la plus grande attention.
+      Nous commençons par examiner les étiquettes de soin pour comprendre les exigences spécifiques de chaque vêtement. Si nécessaire, nous effectuons un prétraitement pour enlever les taches et les saletés tenaces. Nous utilisons des produits de nettoyage écologiques pour préserver la qualité et la couleur du tissu.
+      Après le nettoyage, nous passons chaque vêtement à travers un processus de repassage minutieux pour assurer un fini lisse et sans plis. Si un vêtement est particulièrement délicat, nous utilisons un chiffon de protection pour éviter les dommages pendant le repassage.
   </div>
   <div class="row">
     <div class="col-md-6 order-md-2">
@@ -14,7 +16,11 @@
     </div>
     <div class="col-md-6 order-md-1">
       <h2>Couture</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi commodo tellus a augue tempor, quis sagittis eros eleifend. Fusce tristique bibendum turpis in lacinia. Nulla sed purus vel augue sagittis maximus. Sed posuere ultricies purus, ac eleifend ex gravida in. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque euismod enim non risus volutpat vehicula. In hac habitasse platea dictumst.</p>
+      <p>
+      Chez Pressim, nous sommes également en mesure de prendre en charge tous vos besoins en matière de couture. Nous avons une équipe de professionnels expérimentés qui peut traiter tout, de la simple réparation de bouton à la modification complexe de la taille ou du style de vos vêtements.
+      Nous commençons par discuter avec vous pour comprendre vos besoins spécifiques et pour vous conseiller sur les meilleures options de couture pour vos vêtements. Nous utilisons ensuite des machines à coudre de pointe et des fils de haute qualité pour effectuer les réparations ou les modifications nécessaires.
+      Notre équipe de couturiers expérimentés peut gérer tous les types de tissus, des plus délicats aux plus robustes, pour s'assurer que les modifications sont effectuées correctement et que le vêtement reste en parfait état.
+      </p>
     </div>
   </div>
 </section>

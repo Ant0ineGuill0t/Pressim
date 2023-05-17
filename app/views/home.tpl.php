ecc08dde06eac56a29f5e1be62345ecc61f8e70c
Antoine <a-guillot@laposte.net>
2023-05-17 11:06:17 +0200

    suppression texte

--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -8,9 +8,9 @@
       <h2>Nettoyage & repassage</h2>
       <p class="text-justify">
       Lorsque vous confiez vos vêtements à Pressim, notre équipe de professionnels expérimentés s'assure que chaque article est traité avec le plus grand soin et la plus grande attention.
-      Nous commençons par examiner les étiquettes de soin pour comprendre les exigences spécifiques de chaque vêtement. Si nécessaire, nous effectuons un prétraitement pour enlever les taches et les saletés tenaces. Nous utilisons des produits de nettoyage écologiques pour préserver la qualité et la couleur du tissu.
-      Après le nettoyage, nous passons chaque vêtement à travers un processus de repassage minutieux pour assurer un fini lisse et sans plis. Si un vêtement est particulièrement délicat, nous utilisons un chiffon de protection pour éviter les dommages pendant le repassage.
-  </div>
+      Nous commençons par examiner les étiquettes de soin pour comprendre les exigences spécifiques de chaque vêtement. Si nécessaire, nous effectuons un prétraitement pour enlever les taches et les saletés tenaces.
+      </p>
+    </div>
   <div class="row">
     <div class="col-md-6 order-md-2">
       <img src="<?= $viewData['baseUri']; ?>/assets/images/couture.jpg" alt="Photo de l'article 2" class="img-fluid">
@@ -19,8 +19,7 @@
       <h2>Couture</h2>
       <p class="text-justify">
       Chez Pressim, nous sommes également en mesure de prendre en charge tous vos besoins en matière de couture. Nous avons une équipe de professionnels expérimentés qui peut traiter tout, de la simple réparation de bouton à la modification complexe de la taille ou du style de vos vêtements.
-      Nous commençons par discuter avec vous pour comprendre vos besoins spécifiques et pour vous conseiller sur les meilleures options de couture pour vos vêtements. Nous utilisons ensuite des machines à coudre de pointe et des fils de haute qualité pour effectuer les réparations ou les modifications nécessaires.
-      Notre équipe de couturiers expérimentés peut gérer tous les types de tissus, des plus délicats aux plus robustes, pour s'assurer que les modifications sont effectuées correctement et que le vêtement reste en parfait état.
+      Nous commençons par discuter avec vous pour comprendre vos besoins spécifiques et pour vous conseiller sur les meilleures options de couture pour vos vêtements.
       </p>
     </div>
   </div>
